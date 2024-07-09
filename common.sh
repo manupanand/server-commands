683d2fd3670387c5b0a40292015dfde445afe5f2
manupanand <manupanandindianinvestor@gmail.com>
2024-07-09 22:59:37 +0530

    new scripts variables and function

--- common.sh
+++ common.sh
@@ -1,22 +1,34 @@
 
+LOG_FILE=/var/tmp/install.log
 
+PRINT(){
+  echo  &>> $LOG_FILE
+  echo  &>> $LOG_FILE
+  echo "####################### $* ##########################" &>> $LOG_FILE
+  echo $*
+}
 # install golang
-
+PRINT Installing golang
 goLang(){
-  sudo dnf install ${library} -y
+  sudo dnf install ${library} -y &>> $LOG_FILE
   go version
 
 }
-installNginx(){
-  sudo dnf update -y
-  sudo dnf install dnf-utils -y
-#copy repo file
-  cp ${library}.repo /etc/yum.repos.d/${library}.repo
-#install nginx
-sudo dnf config-manager --set-enabled ${library}-mainline
+# install nginx
 
-sudo dnf install ${library} -y
-sudo systemctl enable ${library}
-sudo systemctl start ${library}
-sudo systemctl status ${library}
+installNginx(){
+  PRINT updating dnf-utils
+  sudo dnf update -y &>>$LOG_FILE
+  sudo dnf install dnf-utils -y &>> $LOG_FILE
+#copy repo file
+PRINT copying nginx-repo
+  cp ${library}.repo /etc/yum.repos.d/${library}.repo &>> $LOG_FILE
+#install nginx
+PRINT Installing nginx
+sudo dnf config-manager --set-enabled ${library}-mainline &>> $LOG_FILE
+
+sudo dnf install ${library} -y &>> $LOG_FILE
+sudo systemctl enable ${library} &>> $LOG_FILE
+sudo systemctl start ${library} &>> $LOG_FILE
+sudo systemctl status ${library} &>> $LOG_FILE
 }
