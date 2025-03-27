870018c496ef561f64a384de0cbd38320ac40a8a
manupanand <manupanandindianinvestor@gmail.com>
2025-03-27 09:29:02 +0530

    update dnf and aptupdate

--- aws-run.sh
+++ aws-run.sh
@@ -11,3 +11,19 @@ echo "UsePAM yes" | sudo tee -a /etc/ssh/sshd_config
 
 # Restart SSH service
 sudo systemctl restart sshd
+
+
+#!/bin/bash
+
+# Set the password for "ec2-user" (USE WITH CAUTION)
+echo "ec2-user:YourSecurePassword" | sudo chpasswd
+
+# Enable password authentication in SSH
+sudo sed -i 's/^#\?\(PasswordAuthentication\) no/\1 yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?\(ChallengeResponseAuthentication\) no/\1 yes/' /etc/ssh/sshd_config
+
+# Ensure UsePAM is enabled (modify instead of appending)
+sudo sed -i 's/^#\?\(UsePAM\) no/\1 yes/' /etc/ssh/sshd_config
+
+# Restart SSH service to apply changes
+sudo systemctl restart sshd
